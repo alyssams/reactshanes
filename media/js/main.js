a8d6a8d9f5a856e8c8ff3ea0bf11856cf2adfa0b
Alyssa Saucedo <asilla7@gmail.com>
2016-03-07 15:54:19 -0800

    initial commit
    first upload

---
+++ media/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+    $(".zoom-container a")
+    .each(function(i) {
+      if (i != 0) {
+        $("#beep-two")
+          .clone()
+          .attr("id", "beep-two" + i)
+          .appendTo($(this).parent());
+      }
+      $(this).data("beeper", i);
+    })
+    .mouseenter(function() {
+      $("#beep-two" + $(this).data("beeper"))[0].play();
+    });
+  	$("#beep-two").attr("id", "beep-two0");
+})(jQuery);
