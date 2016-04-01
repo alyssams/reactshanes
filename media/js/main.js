04ca189318106cf6b06b45cbf33f8061f7b661be
Alyssa Saucedo <asilla7@gmail.com>
2016-03-31 23:26:05 -0700

    fixed for mobile

--- media/js/main.js
+++ media/js/main.js
@@ -1,39 +1,46 @@
 (function($) {
-	$('.zoom-container').mouseenter(function() {
-        $(this).find('audio').removeClass('inactive');
-        $(this).find('audio').addClass('active');
-        $(this).find('audio')[0].play();
-    });
-    $('.zoom-container').mouseout(function () {
-        $(this).find('audio').removeClass('active');
-        $(this).find('audio').addClass('inactive');
-    });
+	var $window = $(window);
+    var $html = $('html');
+
+    $window.resize(function resize(){
+        if ($window.width() < 768) {
+            $html.removeClass('no-mobile');
+            return $html.addClass('mobile');
+        }else {
+        	$html.removeClass('mobile');
+        	return $html.addClass('no-mobile');
+        }
+        $html.removeClass('mobile');
+    }).trigger('resize');
 
 
+    if($('html').hasClass('mobile')) {
+    	$('.zoom-container, .zoom-caption').removeClass('has-hover');
+    	var $yo = $('.zoom-container').find('a');
+    	$('.zoom-container a').on('click',function(e) {
+    		e.preventDefault();
+	        $(this).find('.zoom-caption').addClass('inactive');
+	        $(this).find('.reaction').addClass('active');
+	        $(this).find('audio')[0].play();
+	    });
+	    $("a[href]").click(function() {
+		    var url = this.href;
+		    setTimeout(function () {
+		        location.href = url;
+		    }, 2000);
+		    
+		});
+    }
 
-	// function handler(ev) {
-	// 	var target = $(ev.target);
-	// 	var elId = target.attr('id');
-	// 	// var sound = $(".sound")[0];
-	// 	if( target.is(".zoom-caption") ) {
-	// 		console.log('The mouse was over '+ elId );
-
-			
-	// 		// $(elId).mouseenter(function() {
-	// 		// 	sound.pause();
-	// 		// 	sound.play();
-	// 		// });
-
-
-	// 	  // $(elId)[0].play();
-	// 	  // $("#sound-" + Math.ceil(Math.random() * 3))[0].play();
-	// 	   // elId.mouseenter(function(ev) {
-	// 	   //   $("#sound-" + Math.ceil(Math.random() * 3))[0].play();
-	// 	   // });  
-	// 	}
-	// }
-	// $(".zoom-caption").mouseenter(handler);  
- 	
-})(jQuery);
-
-
+    if($('html').hasClass('no-mobile')) {
+		$('.zoom-container a').mouseenter(function() {
+	        $(this).find('audio').removeClass('inactive');
+	        $(this).find('audio').addClass('active');
+	        $(this).find('audio')[0].play();
+	    });
+	    $('.zoom-container a').mouseout(function () {
+	        $(this).find('audio').removeClass('active');
+	        $(this).find('audio').addClass('inactive');
+	    });
+	}
+})(jQuery); 
